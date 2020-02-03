8a9ac8cd7ba3f750980ba487b9a1669b46218497
mapkozelek <60548910+mapkozelek@users.noreply.github.com>
2020-02-03 11:51:47 +0000

    Update randomlyric.js

--- randomlyric.js
+++ randomlyric.js
@@ -1,3 +1,12 @@
-$(function() {
-  $(".various" + new Date().getTime() % 5).css("display", "block");
-});
+jQuery(document).ready(function($) {
+		if (  $('.quote-loop').length ){
+		(function loop() {
+			$('.quote-loop').each(function() {
+				var $self = $(this);
+				$self.parent().queue(function (n) {
+					$self.fadeIn(500).delay(2000).fadeOut(500, n);
+				});
+			}).parent().promise().done(loop);
+		}());
+	}	
+}); 
